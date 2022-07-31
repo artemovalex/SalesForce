a4140f3662526649af12116544804702efe5577e
Alexander Artemov <artemov_alex@mail.ru>
2022-07-31 17:25:31 +0300

    Added new test

--- src/main/java/by/teachmeskills/pages/AccountsPage.java
+++ src/main/java/by/teachmeskills/pages/AccountsPage.java
@@ -1,12 +1,12 @@
 package by.teachmeskills.pages;
 
+import lombok.Data;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 public class AccountsPage extends BasePage {
 
     @FindBy(xpath = "//div[@title='New']")
